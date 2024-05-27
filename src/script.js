ffc34155b923bccf24a6251ac86db0494fce20fc
S-Qui <147270652+S-Qui@users.noreply.github.com>
2024-05-27 16:44:01 +0300

    hymn

---
+++ src/script.js
@@ -0,0 +1,22 @@
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    // Close all other sections
+    for (var j = 0; j < coll.length; j++) {
+      if (coll[j] !== this) {
+        coll[j].classList.remove("active");
+        coll[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
+    
+    // Toggle the current section
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
